d76bf85f55c127e9e0d184c64555ded48a01ded9
Emumba <muneeb.urrehman@emumba.com>
2026-01-07 10:04:03 +0500

    Fixes

--- src/views/components/index.ts
+++ src/views/components/index.ts
@@ -1,4 +1,3 @@
-// Global components
 export * from './button';
 export * from './conditional';
 export * from './container';
@@ -9,7 +8,4 @@ export * from './icon';
 export * from './text';
 export * from './toast';
 export * from './video-player';
-
-// Movie components
 export * from './movie-card';
-export * from './movie-list';
--- src/views/components/movie-list/types.ts
+++
@@ -1,9 +0,0 @@
-import { Movie } from '../../../types/movie.types';
-
-export interface MovieListProps {
-  title: string;
-  movies: Movie[];
-  onMoviePress: (movieId: number) => void;
-  isLoading?: boolean;
-  showFavouriteToggle?: boolean;
-}
