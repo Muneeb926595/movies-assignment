3934dfacc1cc7ca615111d41de62230551493a4a
Emumba <muneeb.urrehman@emumba.com>
2026-01-08 16:40:22 +0500

    Inline fix

--- src/views/screens/movies/components/movie-cast-card/index.tsx
+++ src/views/screens/movies/components/movie-cast-card/index.tsx
@@ -1,5 +1,10 @@
-import FastImage from '@d11/react-native-fast-image';
-import { CastCard, CastCharacter, CastName, CastPlaceholder } from './styles';
+import {
+  CastCard,
+  CastCharacter,
+  CastImage,
+  CastName,
+  CastPlaceholder,
+} from './styles';
 import { getImageUrl } from '../../../../../api';
 import { Cast } from '../../../../../types';
 import { FadeInDown } from 'react-native-reanimated';
@@ -14,15 +19,9 @@ export const MovieCastCard = ({
   return (
     <CastCard key={item.id} entering={FadeInDown.delay(index * 50).springify()}>
       {item.profile_path ? (
-        <FastImage
+        <CastImage
           source={{
             uri: getImageUrl(item.profile_path, 'w185') || '',
-          }}
-          style={{
-            width: 100,
-            height: 100,
-            borderRadius: 8,
-            marginBottom: 8,
           }}
           resizeMode="cover"
         />
